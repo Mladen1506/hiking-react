f00fddb22b0130f424b947370291f423db4b365e
Mladen Makivic <makivic.mladen1203@gmail.com>
2022-07-19 18:47:14 -0400

    update

--- react-app/src/utils/config.js
+++ react-app/src/utils/config.js
@@ -1,5 +1,26 @@
-const config = {};
+// const API_URL_PREFIX = 'http://localhost:3001'; // URL prefix of API urls
+const _make_url_prefix = () => {
+  let url_prefix;
+  if (window.location.host === "localhost:3000") {
+    // CASE localhost react development server
+    // port 3000 is local react development server
+    // port 3001 is default express server port
+    url_prefix = 'http://localhost:3001';
+    // example 'http://localhost:3001'
+  } else {
+    // CASE react app build is hosted inside public folder on real backend
+    // no port changes because port is the same for frontend and backend
+    url_prefix = window.location.protocol + '//' + window.location.host;
+    // example 'http://mywebsite.com'
+    // example 'http://localhost:3001'
+    // example 'http://mywebsite.com:3001'
+  }
+  return url_prefix;
+};
 
+const config = {};
+// config.apiUrlPrefix = API_URL_PREFIX;
+config.apiUrlPrefix = _make_url_prefix();
 config.TOKEN_HEADER_KEY = 'x-hiking-token';
 config.TOKEN_LOCALSTORAGE_KEY = 'hiking_token';
 config.BACKEND_PORT = '3001';
--- react-app/src/utils/url-lib.js
+++ react-app/src/utils/url-lib.js
@@ -2,10 +2,25 @@ import config from "./config";
 
 export const urlLib = {};
 
-
+/*
 urlLib.urlPrefix = () => {
   // return 'http://localhost:3001';
   return 'http://localhost:' + config.BACKEND_PORT
+};
+*/
+// FIX ZA HEROKU
+urlLib.websitePrefix = () => {
+  // WEBSITE PREFIX UKLJUCUJE CEO POCETAK URL-a (i podomen i domen) NA KOJEM SE HOSTUJE SAJT
+  let url = ''; // moze da bude i 'http://mysite.com' kasnije
+
+  return url;
+};
+
+apiLib.apiPrefix = () => {
+  // API PREFIX UKLJUCUJE CEO POCETAK URL-a (i podomen i domen) NA KOJEM SE HOSTUJE API
+  // let url = 'api.sajtzaapi.com'; // TODO...
+  let url = config.apiUrlPrefix;
+  return url;
 };
 
 urlLib.apiGraphQL = () => {
